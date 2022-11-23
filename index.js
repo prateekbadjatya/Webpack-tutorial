cfbd4f12b8ad3922a79d8220231f7605d94cb000
prateekbadjatya <prateek.badjatya@gmail.com>
2022-11-24 00:08:00 +0530

    day 2 webpack config file creation

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import _ from 'lodash';
-
-function buttonClicked() {
-const element = document.getElementById("header");
- element.innerHTML = 'Hello World!'
-
- const listItems = ["Apple", "Orange", "banana"]
- const ul = document.getElementById("shoppinglist")
-    _.forEach(listItems, function (item) {
-    const tempElement = document.createElement("li")
-    tempElement.innerHTML = item
-    ul.appendChild(tempElement)
-    });
-}
+document.getElementById("button1").addEventListener("click", function () {
+    const element = document.getElementById("header");
+    element.innerHTML = 'Hello World!'
+   
+    const listItems = ["Apple", "Orange", "banana"]
+    const ul = document.getElementById("shoppinglist")
+       _.forEach(listItems, function (item) {
+       const tempElement = document.createElement("li")
+       tempElement.innerHTML = item
+       ul.appendChild(tempElement)
+       });
+})
