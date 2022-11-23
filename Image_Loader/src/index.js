e7c26b0aa62fdd81fc01cc0084bd3d5fa8eef6ad
prateekbadjatya <prateek.badjatya@gmail.com>
2022-11-24 02:17:35 +0530

    image loader

---
+++ Image_Loader/src/index.js
@@ -0,0 +1,32 @@
+import style from "./index.scss";
+
+import "./ClearButton";
+import _ from "lodash";
+
+import logo from '../assets/download.png'
+
+// console.log("=====", style)
+
+const btn1 = document.getElementById("button1")
+
+const logos = document.getElementById("logo")
+
+document.getElementById("button1").addEventListener("click", function () {
+  const element = document.getElementById("header");
+  element.innerHTML = "Hello World!";
+
+  const listItems = ["Apple", "Orange", "banana"];
+  const ul = document.getElementById("shoppinglist");
+  _.forEach(listItems, function (item) {
+    const tempElement = document.createElement("li");
+    tempElement.innerHTML = item;
+    ul.appendChild(tempElement);
+  });
+});
+
+logos.src= logo
+
+btn1.classList.add(style.button)
+//all the global styles reove from the style oobject
+// btn1.classList.add(["button"])
+
---
+++ Image_Loader/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext]",
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: { modules: true }
+          }
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: { modules: true },
+          },
+          {
+            loader: "sass-loader"
+          }
+        ],
+        // <<<------read direction
+        // use: ["style-loader", "css-loader", " "sass-loader","],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
